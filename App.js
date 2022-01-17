49a60625a3ee933c9f645fe13002d166d7b8b12c
yeyeyeeeejin <79955539+yeyeyeeeejin@users.noreply.github.com>
2022-01-18 01:35:48 +0900

    layout with flex first

--- App.js
+++ App.js
@@ -26,42 +26,17 @@ import { Component, Fragment } from 'react/cjs/react.production.min';
 class App extends Component {
   render(){
     return (
-      <View>
-        <View style ={styles.container}>
-        <Image source={require('./assets/img.jpg')} style={{width:150,height:150}}></Image>
-        
-        <View style={{flexDirection : 'column'}}>
-        <View style={{flexDirection:'row'}}>
-        <Text style={styles.title}>활동연대 </Text>
-        <Text style={styles.detail}>2010, 2020</Text>
-        </View>
-        <View style={{flexDirection:'row'}}>
-        <Text style={styles.title}>출생 </Text>
-        <Text style={styles.detail}>1989년 7월 23일</Text>
-        </View>
-        <View style={{flexDirection:'row'}}>
-        <Text style={styles.title}>활동유형 </Text>
-        <Text style={styles.detail}> 남성 솔로</Text>
-        </View>
-        <View style={{flexDirection:'row'}}>
-        <Text style={styles.title}>활동장르 </Text>
-        <Text style={styles.detail}>R&B rap</Text>
-        </View>
-        <View style={{flexDirection:'row'}}>
-        <Text style={styles.title}>데뷔 </Text>
-        <Text style={styles.detail}>2016년 싱글 앨범 [Wavy]</Text>
-        </View>
-        <View style={{flexDirection:'row'}}>
-        <Text style={styles.title}>활동 </Text>
-        <Text style={styles.detail}>LATELY(Feat.Hoody)</Text>
-        </View>
-        </View>
-        </View>
-        <View style = {{width : 50,height :50, backgroundColor : 'lightblue'}}></View>
-        <View style = {{width : 100,height :100, backgroundColor : 'skyblue'}}></View>
-        <View style = {{width : 150,height :150, backgroundColor : 'steelblue'}}></View>
+  /*       <View style = {styles.flexcontainer}>
+        <View style = {{flex : 1, backgroundColor : 'lightblue'}}></View>
+        <View style = {{flex:2, backgroundColor : 'skyblue'}}></View>
+        <View style = {{flex:3, backgroundColor : 'steelblue'}}></View>
+        </View> */
+        <SafeAreaView style = {styles.flexcontainer}>
+        <View style = {{width:50, height:50, backgroundColor : 'lightblue'}}></View>
+        <View style = {{width:50, height:50, backgroundColor : 'skyblue'}}></View>
+        <View style = {{ height:50, backgroundColor : 'steelblue'}}></View>
+        </SafeAreaView>
 
-      </View>
     );
   }
   
@@ -83,6 +58,14 @@ const styles = StyleSheet.create({
   detail : {
     marginLeft : 10,
     color : 'black',
+  },
+  flexcontainer : {
+    flexDirection:'column',
+    //justifyContent : 'space-between',//sapce-around..
+    //flexDirection:'column-reverse',
+    alignItems : 'center',
+    justifyContent : 'center',
+    flex:1,
   }
   
 });
